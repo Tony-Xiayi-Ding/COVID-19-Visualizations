6308a2b5624b78cf00d444142458874d48e4e0a8
Tony Ding <78240133+Tony-Xiayi-Ding@users.noreply.github.com>
2025-05-07 19:48:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,11 +82,6 @@ text_0 = "Visualizing the Impact of COVID-19 and the Vaccination Data in 2021"
 st.write(f'<div style="text-align: center; font-size: 42px">{text_0}</div>', unsafe_allow_html=True)
 #st.write("## Visualizing the Impact of COVID-19 and the Vaccination Statuses in 2021")
 
-#text_1 = "Group: Viz or DY"
-#text_2 = "Members: Tony Ding, Chen Yang"
-#st.write(f'<div style="text-align: right; font-size: 24px">{text_1}</div>', unsafe_allow_html=True)
-#st.write(f'<div style="text-align: right; font-size: 24px">{text_2}</div>', unsafe_allow_html=True)
-
 st.write("Please note:")
 st.write("  1. All visualizations presented below are built on data from Jan. 12nd, 2021 to Dec. 30th, 2021. ")
 st.write("  2. Case Fatality Rate for COVID-19 is calculated as the number of deaths from COVID-19 divided by the number of confirmed cases of COVID-19.")
